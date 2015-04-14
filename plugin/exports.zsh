1467f774ca8aa95e67991e1c9f3ae0086df577eb
Evan Sosenko <razorx@evansosenko.com>
2015-04-14 15:10:17 -0700

    Add comment to plugin/exports.zsh.

--- plugin/exports.zsh
+++ plugin/exports.zsh
@@ -5,6 +5,7 @@ if [[ -d /opt/android-sdk/tools ]]; then
   PATH=$PATH:/opt/android-sdk/tools
 fi
 
+# Add Android platform tools to path.
 if [[ -d /opt/android-sdk/platform-tools ]]; then
   PATH=$PATH:/opt/android-sdk/platform-tools
 fi

9089157abd034f440b8b05888acda1240eec1033
Evan Sosenko <razorx@evansosenko.com>
2015-04-20 16:21:43 -0700

    Add ~/.local/bin to path.

--- plugin/exports.zsh
+++ plugin/exports.zsh
@@ -10,6 +10,11 @@ if [[ -d /opt/android-sdk/platform-tools ]]; then
   PATH=$PATH:/opt/android-sdk/platform-tools
 fi
 
+# Add local binaries to path.
+if [[ -d ~/.local/bin ]]; then
+  PATH=$HOME/.local/bin:$PATH
+fi
+
 # Add Cabal installed binaries to path.
 if [[ -d ~/.cabal ]]; then
   PATH=$HOME/.cabal/bin:$PATH

cffd4c0617096481fe2861b9b3a7c51bf945e6db
Evan Sosenko <razorx@evansosenko.com>
2015-05-06 16:07:31 -0700

    Fix loading exports.

--- plugins.zsh
+++ plugins.zsh
@@ -1,7 +1,7 @@
 if ! zgen saved; then
   echo "Creating a zgen save".
 
-  zgen load razor-x/zshrc plugin/exports.zsh
+  zgen load razor-x/zshrc plugin exports
   zgen load razor-x/zshrc plugin
   zgen oh-my-zsh
 

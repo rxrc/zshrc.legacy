1c9f25ab724ea77a13252ce6d199388bc7570e82
Evan Sosenko <razorx@evansosenko.com>
2015-05-06 15:58:05 -0700

    Load exports.zsh before all plugins.

--- plugin/zshrc.zsh
+++ plugin/zshrc.zsh
@@ -1,8 +1,4 @@
-# Set and create cache directory.
-if [[ -z "$XDG_CACHE_HOME" ]]; then
-  XDG_CACHE_HOME=$HOME/.cache
-fi
-
+# Create cache directory.
 mkdir -p $XDG_CACHE_HOME/zsh
 
 # Set history file.

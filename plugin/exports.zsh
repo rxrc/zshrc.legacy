f0c33cd3271185e96fc610f344e15446e6fa9a76
Evan Sosenko <razorx@evansosenko.com>
2015-05-06 15:45:56 -0700

    Add XDG Base Directory Specification.

--- plugin/exports.zsh
+++ plugin/exports.zsh
@@ -1,4 +1,12 @@
+# Editor.
 export EDITOR='vim'
+
+# XDG Base Directory Specification.
+export XDG_DATA_HOME=$HOME/.local/share
+export XDG_CONFIG_HOME=$HOME/.config
+export XDG_DATA_DIRS=/usr/local/share/:/usr/share/
+export XDG_CONFIG_DIRS=/etc/xdg
+export XDG_CACHE_HOME=$HOME/.cache
 
 # Add Android tools to path.
 if [[ -d /opt/android-sdk/tools ]]; then

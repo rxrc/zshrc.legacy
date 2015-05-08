d60fe3af5528836e50440fbc44765e13676c850b
Evan Sosenko <razorx@evansosenko.com>
2015-05-07 20:25:46 -0700

    Add mknew function.

--- plugin/functions-git.zsh
+++ plugin/functions-git.zsh
@@ -17,3 +17,22 @@ function gitsed () {
 
   git ls-files -z | xargs -0 sed -i $1
 }
+
+# Create a new project from https://github.com/makenew.
+function mknew () {
+  if ! [[ -n "$1" ]]; then
+    echo 'Must specify repo.'
+    return 1
+  fi
+
+  if ! [[ -n "$2" ]]; then
+    echo 'Must specify name.'
+    return 1
+  fi
+
+  git clone git@github.com:makenew/$1.git $2
+  cd $2
+  git remote rename origin upstream
+  git remote set-url upstream https://github.com/makenew/$1.git
+  git fetch --no-tags upstream
+}

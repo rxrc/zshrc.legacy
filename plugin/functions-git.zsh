3f10837d5487be90845c771503bd5f402f01485f
Evan Sosenko <razorx@evansosenko.com>
2015-05-07 20:18:55 -0700

    Add arg checks to some functions.

--- plugin/functions-git.zsh
+++ plugin/functions-git.zsh
@@ -1,9 +1,19 @@
 # Clone GitHub repository.
 function ghcl () {
+  if ! [[ -n "$1" ]]; then
+    echo 'Must specify repo.'
+    return 1
+  fi
+
   git clone git@github.com:$1.git
 }
 
 # Run sed in-place on all tracked files.
 function gitsed () {
+  if ! [[ -n "$1" ]]; then
+    echo 'Must specify sed command.'
+    return 1
+  fi
+
   git ls-files -z | xargs -0 sed -i $1
 }
--- plugin/functions.zsh
+++ plugin/functions.zsh
@@ -11,12 +11,22 @@ function pyserver () {
 
 # Generate an SSL certificate for test use only.
 function sslgen-cert-test () {
+  if ! [[ -n "$1" ]]; then
+    echo 'Must specify name to use.'
+    return 1
+  fi
+
   openssl req -new -x509 -nodes -newkey rsa:2048 -keyout $1.key -out $1.pem -days 36524 \
     -subj "/C=US/ST=California/L=San Francisco/O=Example Inc./CN=$1/emailAddress=webmaster@example.com"
 }
 
 # Create a new tar archive.
 function tarz () {
+  if ! [[ -n "$1" ]]; then
+    echo 'Must specify directory to use.'
+    return 1
+  fi
+
   tar -czf $1.tar.gz $1
 }
 

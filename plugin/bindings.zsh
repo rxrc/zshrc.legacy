6d6fdae370f29d9701d5c378ab91ab3e3c2aa939
Evan Sosenko <razorx@evansosenko.com>
2016-01-01 15:41:27 -0800

    Add ^P and ^N binds for history-substring-search

--- plugin/bindings.zsh
+++ plugin/bindings.zsh
@@ -1,2 +1,7 @@
+# Basic word movement.
 bindkey "^F" forward-word
 bindkey "^R" backward-word
+
+# Additional binds for history-substring-search.
+bindkey "^P" history-substring-search-up
+bindkey "^N" history-substring-search-down
